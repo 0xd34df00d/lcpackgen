f8a91e31739bc0313c1ef40a114014811fce07b6
0xd34df00d <0xd34df00d@gmail.com>
2011-10-30 16:39:11 +0400

    License header.

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1,3 +1,21 @@
+/**********************************************************************
+ * LC Package Generator - a small utility to create LackMan packages.
+ * Copyright (C) 2010-2011  Georg Rudoy
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ **********************************************************************/
+
 #include "mainwindow.h"
 #include <stdexcept>
 #include <QStandardItemModel>
@@ -454,11 +472,11 @@ void MainWindow::on_ActionSave__triggered ()
 			!rawScreens.isEmpty ())
 	{
 		QDomElement images = doc.createElement ("images");
-		
+
 		QDomElement icon = doc.createElement ("icon");
 		icon.setAttribute ("url", iconUrl);
 		images.appendChild (icon);
-		
+
 		QStringList splitted = rawThumbs.split ("\n", QString::SkipEmptyParts);
 		Q_FOREACH (const QString& thumbUrl, splitted)
 		{
--- src/mainwindow.h
+++ src/mainwindow.h
@@ -1,3 +1,21 @@
+/**********************************************************************
+ * LC Package Generator - a small utility to create LackMan packages.
+ * Copyright (C) 2010-2011  Georg Rudoy
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ **********************************************************************/
+
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 #include <QMainWindow>
@@ -9,7 +27,7 @@ class QStandardItemModel;
 class MainWindow : public QMainWindow
 {
 	Q_OBJECT
-	
+
 	Ui::MainWindow Ui_;
 	QLabel *ValidLabel_;
 	QStandardItemModel *VersModel_;
